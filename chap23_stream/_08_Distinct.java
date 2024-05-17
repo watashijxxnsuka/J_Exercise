cd6e2af313deb8d9de1ced6c25f182803c3ae55f
watashijxxnsuka <jxxn2538@gmail.com>
2024-05-17 17:45:38 +0900

    24.05.17

---
+++ chap23_stream/_08_Distinct.java
@@ -0,0 +1,22 @@
+package chap23_stream;
+
+import java.util.Arrays;
+
+public class _08_Distinct {
+	public static void main(String[] args) {
+		
+		int[] intArr = new int[10];
+		
+		for(int i = 0; i < intArr.length; i++) {
+			intArr[i] = (int)(Math.random() * 10) + 1 ;
+		}
+		
+		// 1. distinct(): 중복 값이 제거된 새로운 Stream 을 리턴.(중간단계 메소드)
+		// 객체의 경우에는 주소값이 같으면 중복으로 인식
+		Arrays.stream(intArr)
+				.distinct()
+				.forEach(num -> System.out.println(num));
+		
+	}
+
+}
--- chap23_stream/car/HyundaiCar.java
+++ chap23_stream/car/HyundaiCar.java
@@ -36,5 +36,10 @@ public class HyundaiCar {
 		return "HyundaiCar [model=" + model + ", price=" + price + "]";
 	}
 	
+	public void carInfo() {
+		System.out.println("모델: " + this.model);
+		System.out.println("가격: " + this.price);
+	}
+	
 	
 }
